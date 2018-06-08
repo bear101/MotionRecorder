347e96319aa7f1d8b99609e629d864c67115c5cf
Bjørn Damstedt Rasmussen <contact@bearware.dk>
2018-06-08 08:45:54 +0200

    macOS env

--- env.sh
+++ env.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$PWD/libs/ffmpeg/lib:$PWD/libs/opencv/lib:$LD_LIBRARY_PATH
-export PKG_CONFIG_PATH=$PWD/libs/ffmpeg/lib/pkgconfig:$PKG_CONFIG_PATH
-export PKG_CONFIG_LIBDIR=$PWD/libs/ffmpeg/lib
+if [ "Darwin" = `uname` ]; then
+    export DYLD_LIBRARY_PATH=$PWD/libs/ffmpeg/lib:$PWD/libs/opencv/lib:$DYLD_LIBRARY_PATH
+else
+    export LD_LIBRARY_PATH=$PWD/libs/ffmpeg/lib:$PWD/libs/opencv/lib:$LD_LIBRARY_PATH
+    export PKG_CONFIG_PATH=$PWD/libs/ffmpeg/lib/pkgconfig:$PKG_CONFIG_PATH
+    export PKG_CONFIG_LIBDIR=$PWD/libs/ffmpeg/lib
+fi
